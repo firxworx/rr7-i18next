1ece61fad531979f963bd9670b57825095c58f10
Kevin Firko <kfirko@bitcurve.com>
2025-03-10 18:58:19 -0400

    chore: vite config w/ hono server and optimizeDeps fix for i18next-fs-backend

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,29 @@ import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { reactRouterHonoServer } from 'react-router-hono-server/dev'
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouterHonoServer({
+      runtime: 'node',
+      serverEntryPoint: 'app/server/index.ts',
+      dev: {
+        export: 'default',
+        // exclude: [...],
+      },
+    }),
+    reactRouter(),
+    tsconfigPaths(),
+  ],
+  // resolution for issue similar to https://github.com/vitejs/vite/issues/6985
+  optimizeDeps: {
+    include: ['i18next-fs-backend'],
+    esbuildOptions: {
+      supported: {
+        'top-level-await': true,
+      },
+    },
+  },
 })
