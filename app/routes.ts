d415070befa2fef85fd4440a38a98c93bce6e2ba
Kevin Firko <kfirko@bitcurve.com>
2025-03-10 18:56:33 -0400

    feat: add routes to rr7 routes export

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,12 @@
-import { type RouteConfig, index } from '@react-router/dev/routes'
+import { type RouteConfig, index, layout, route } from '@react-router/dev/routes'
 
-export default [index('routes/home.tsx')] satisfies RouteConfig
+/**
+ * Routes configuration.
+ *
+ * Refer to `locale.index.tsx` route for key i18n business logic.
+ */
+export default [
+  route(':locale?', 'routes/locale.index.tsx', [
+    layout('routes/_layout.tsx', [index('routes/root/home.tsx'), route('about', 'routes/root/about.tsx')]),
+  ]),
+] satisfies RouteConfig
