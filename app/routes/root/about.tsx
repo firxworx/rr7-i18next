6885620f7fb76a5177a90ad1e526deb618df7911
Kevin Firko <kfirko@bitcurve.com>
2025-03-10 19:24:26 -0400

    feat: expand example / i18n welcome

--- app/routes/root/about.tsx
+++ app/routes/root/about.tsx
@@ -1,10 +1,23 @@
+import { useTranslation } from 'react-i18next'
 import type { Route } from './+types/home'
-import { Welcome } from '../../welcome/welcome'
+import { PATHNAMES } from '@/constants'
+import { Link } from 'react-router'
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: 'New React Router App (About)' }, { name: 'description', content: 'About Page' }]
 }
 
 export default function About() {
-  return <div>About Route</div>
+  const { t } = useTranslation('common')
+
+  return (
+    <div className="flex flex-col gap-8">
+      <h1 className="text-4xl font-bold">{t('nav.about')}</h1>
+      <p>
+        <Link to={PATHNAMES.root} className="underline underline-offset-4">
+          {t('action.goToHome')}
+        </Link>
+      </p>
+    </div>
+  )
 }
