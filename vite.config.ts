a49c5568884423576965d51a237163bde1c5306f
Kevin Firko <kfirko@bitcurve.com>
2025-04-17 03:48:22 -0400

    chore: reconfigure vite for cloudflare + remove react-router-hono-server

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,31 @@
-import { reactRouter } from '@react-router/dev/vite'
-import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
+
+import { reactRouter } from '@react-router/dev/vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
+import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { reactRouterHonoServer } from 'react-router-hono-server/dev'
 
 export default defineConfig({
+  esbuild: {
+    target: 'es2022',
+  },
+  // uncomment for debugging only
+  // do not use in production (sourcemaps can make all code visible to public)
+  // build: {
+  //   minify: false,
+  //   sourcemap: true,
+  // },
+  server: {
+    warmup: {
+      clientFiles: ['./app/entry.client.tsx', './app/root.tsx', './app/routes/**/*.tsx'],
+      ssrFiles: ['./app/server/index.ts', './app/entry.server.tsx', './app/routes/**/*.tsx'],
+    },
+  },
   plugins: [
     tailwindcss(),
-    reactRouterHonoServer({
-      runtime: 'node',
-      serverEntryPoint: 'app/server/index.ts',
-      dev: {
-        export: 'default',
-        // exclude: [...],
+    cloudflare({
+      viteEnvironment: {
+        name: 'ssr',
       },
     }),
     reactRouter(),
@@ -21,6 +34,7 @@ export default defineConfig({
   // resolution for issue similar to https://github.com/vitejs/vite/issues/6985
   optimizeDeps: {
     include: ['i18next-fs-backend'],
+    exclude: ['@opentelemetry/*'],
     esbuildOptions: {
       supported: {
         'top-level-await': true,
