1614ebbc2b1524590f09bfe0909c0a119162e22a
Kevin Firko <kfirko@bitcurve.com>
2025-04-17 03:49:18 -0400

    feat: customize hono server to support cloudflare worker deploy

--- app/server/index.ts
+++ app/server/index.ts
@@ -1,48 +1,45 @@
 import { Hono } from 'hono'
 import { prettyJSON } from 'hono/pretty-json'
 import { contextStorage } from 'hono/context-storage'
+import { createRequestHandler } from 'react-router'
 
-import { createHonoServer } from 'react-router-hono-server/node'
-
-import type { ApiContext, Variables } from '@/server/types'
+import type { ApiContext } from '@/server/types'
 import { i18nMiddleware } from '@/server/middleware/i18n.middleware'
 
-/**
- * Declare types for react-router context available to actions and loaders.
- */
-declare module 'react-router' {
-  interface AppLoadContext extends Variables {}
-}
+// references --
+// https://developers.cloudflare.com/workers/runtime-apis/context/ -- cloudflare ExecutionContext
+// https://github.com/cloudflare/templates/blob/staging/react-postgres-fullstack-template/api/index.js - hono integration
+// https://github.com/cloudflare/templates/blob/staging/react-router-postgres-ssr-template/api/index.js - hono/rr integration
+// https://github.com/cloudflare/templates/blob/staging/react-router-starter-template/workers/app.ts - defines rr as middleware
 
-const app = new Hono<ApiContext>().use(contextStorage()).use('*', prettyJSON()).use(i18nMiddleware)
+// https://sergiodxa.com/tutorials/customize-remix-app-load-context-type
+// react-router-hono-server project (also see for the defaultLogger option)
 
-/**
- * @see https://sergiodxa.com/tutorials/customize-remix-app-load-context-type
- */
-export default await createHonoServer<ApiContext>({
-  /**
-   * Custom hono instance.
-   */
-  app,
+const build = await import('virtual:react-router/server-build')
+const requestHandler = createRequestHandler(build, import.meta.env.MODE)
 
-  /**
-   * Callback to populate react-router v7+ (remix) context.
-   */
-  getLoadContext(ctx, { mode, build }) {
-    const isProductionMode = mode === 'production'
-
-    return {
-      // metadata
-      appVersion: isProductionMode ? build.assets.version : 'dev',
-
-      // locale and i18next instance set by i18n middleware
+const app = new Hono<ApiContext>()
+  // chain middleware
+  .use(contextStorage())
+  .use('*', prettyJSON())
+  .use(i18nMiddleware)
+  // react-router
+  .get('*', async (ctx) => {
+    return await requestHandler(ctx.req.raw, {
+      appVersion: import.meta.env.PROD ? build.assets.version : 'dev',
       locale: ctx.get('locale'),
       i18next: ctx.get('i18next'),
-    }
-  },
+      cloudflare: {
+        env: ctx.env,
+        ctx: Object.assign(ctx.executionCtx, { props: undefined }),
+      },
+    })
+  })
+  // catch-all route for static assets
+  .all('*', async (c) => {
+    return c.env.ASSETS.fetch(c.req.raw)
+  })
 
-  /**
-   * Control hono's default logger.
-   */
-  defaultLogger: true,
-})
+export default {
+  fetch: app.fetch,
+} satisfies ExportedHandler<Env>
--- app/server/types.ts
+++ app/server/types.ts
@@ -1,7 +1,16 @@
 import type { SupportedLocale } from '@/constants'
 
-// define bindings for cloudflare or similar deployment environments
-export interface Bindings extends Record<string, never> {}
+/**
+ * Cloudflare service bindings.
+ *
+ * If using node instead of Cloudflare this can be replaced with `interface Bindings extends Record<string, never> {}`.
+ */
+export type Bindings = {
+  ASSETS: Fetcher
+
+  // DB: D1Database
+  // MEDIA: R2Bucket
+} & Env
 
 export interface Variables {
   // meta
@@ -12,7 +21,28 @@ export interface Variables {
   i18next: import('i18next').i18n
 }
 
+/**
+ * Hono context type.
+ */
 export type ApiContext = {
   Bindings: Bindings
   Variables: Variables
+}
+
+/**
+ * Declare types for react-router context available to actions and loaders.
+ *
+ * We are not adopting the new react-router v7 unstable middleware for now as the API is still in flux.
+ */
+declare module 'react-router' {
+  interface Future {
+    unstable_middleware: false
+  }
+
+  export interface AppLoadContext extends Variables {
+    cloudflare: {
+      env: Env
+      ctx: ExecutionContext
+    }
+  }
 }
