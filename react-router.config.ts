eb023e867df89f29344194fcce26860d1e1dd887
Kevin Firko <kfirko@bitcurve.com>
2025-04-17 03:48:46 -0400

    chore: explicit react-router config preferences

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,9 @@ import type { Config } from '@react-router/dev/config'
 
 export default {
   ssr: true,
+  appDirectory: 'app',
+  buildDirectory: 'build',
+  prerender: [],
   future: {
     unstable_optimizeDeps: true,
     unstable_viteEnvironmentApi: true,
